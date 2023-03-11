aa973e876593728d923e5824c3c9b6f957865e2f
Marc0x1 <marco.loletti@zohomail.eu>
2023-03-11 20:15:53 +0100

    .

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,6 +10,8 @@
 
 mollysocket_setup_source() {
     ynh_setup_source -d "$install_dir" -s "$YNH_ARCH"
+    chown -R $app:$app "$install_dir"
+    chmod -R 750 "$install_dir/mollysocket-0.1.0"
 }
 
 #=================================================
